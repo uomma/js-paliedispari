13369e1af1ca09069030a051dbb77a7007c9c9df
uomma <marcelloleone975@gmail.com>
2023-03-02 11:16:18 +0100

    palindroma

--- palidroma/main.js
+++ palidroma/main.js
@@ -1,1 +1,24 @@
-'use: script'
+'use strict';
+    
+
+function palindroma(string) {
+
+  
+    const len = string.length;
+
+
+    for (let i = 0; i < len / 2; i++) {
+
+        
+        if (string[i] !== string[len - 1 - i]) {
+            return 'non è palindroma';
+        }
+    }
+    return 'è palindroma';
+}
+
+const string = prompt('inserisci una parola');
+
+const value = palindroma(string);
+
+console.log(value);
